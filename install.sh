8ac13d36aeddc38349d934e44af6ef5ad162694c
xtacocorex <robert.wolterman@gmail.com>
2016-05-03 20:57:02 -0500

    additional commit for #1, updating the install path

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Robert Wolterman (xtacocorex) 2016
 
 # COPY FILES
-cp ./boot_blink.sh /usr/bin/boot_blink.sh
+cp ./boot_blink.sh /usr/local/bin/boot_blink.sh
 cp ./chip_boot_blink.service /etc/systemd/system/chip_boot_blink.service
 
 # ENABLE THE SERVICE
