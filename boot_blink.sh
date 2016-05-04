f2bd658db247f57b47a6b8dc398f99d7c500e1ee
xtacocorex <robert.wolterman@gmail.com>
2016-05-03 20:40:30 -0500

    fixes issue #1 and updates the second sleep time from 0.25 to 0.1 seconds for a more exciting blink

--- boot_blink.sh
+++ boot_blink.sh
@@ -5,14 +5,12 @@
 # Notifies users that the CHIP is booted an able to be used
 
 blink() {
-    case $@ in
+    case $1 in
         off)
         sudo i2cset -f -y 0 0x34 0x93 0x0
-        shift
         ;;
         on)
         sudo i2cset -f -y 0 0x34 0x93 0x1
-        shift
         ;;
     esac
 }
@@ -22,7 +20,7 @@ while [  $COUNTER -lt 10 ]; do
     blink off
     sleep 0.25
     blink on
-    sleep 0.25 
+    sleep 0.1 
     let COUNTER=COUNTER+1
 done
 
